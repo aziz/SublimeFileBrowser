bce32557a80253d7a620a1d89c0ebb9a140b1898
Vova Kolobok <vovkkk@ya.ru>
2015-01-29 22:39:57 +0600

    Fix #62 (disable refresh on plugin_loaded for ST2)
    also fix dired_show_full_path

--- dired.py
+++ dired.py
@@ -74,6 +74,8 @@ def plugin_loaded():
     if len(sublime.windows()) == 1 and len(sublime.windows()[0].views()) == 0:
         hijack_window()
 
+    if not ST3:
+        return
     for v in sublime.active_window().views():
         if v.settings() and v.settings().get("dired_path"):
             v.run_command("dired_refresh")
@@ -199,7 +201,7 @@ class DiredRefreshCommand(TextCommand, DiredBaseCommand):
             text = [ caption, len(caption)*(u'—') ] if header else []
             if self.view.settings().get('dired_show_full_path', False):
                 view_name = self.view.name()[:2]
-                self.view.set_name(u'%s%s (%s)' % (view_name, caption.split(u'→')[0], path))
+                self.view.set_name(u'%s%s (%s)' % (view_name, name or basename(path), path))
             if not f or self.show_parent():
                 text.append(PARENT_SYM)
             text.extend(f)
